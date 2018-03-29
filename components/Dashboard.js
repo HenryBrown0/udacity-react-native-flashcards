41d56a87273e77afa9bd6b18a629aeec1e727d81
HenryBrown0 <spike.1@live.co.uk>
2018-03-29 14:09:41 +0100

    Add fab button to dashboard

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View, Button, ScrollView } from 'react-native';
 //Redux
 import { connect } from 'react-redux';
 import { fetchDecks } from '../actions';
+//Native base
+import { Icon, Fab } from 'native-base';
 //Components
 import DeckPreview from './DeckPreview';
 
@@ -10,6 +12,9 @@ class Dashboard extends Component {
   static navigationOptions = {
     title: 'Dashboard',
   };
+  state = {
+    active: true,
+  }
 
   componentDidMount(){
 		this.props.fetchDecks();
@@ -36,11 +41,15 @@ class Dashboard extends Component {
             }
           </View>
         </ScrollView>
-        <Button
-          title='New Deck'
-          color="#841584"
-          onPress={() => this.props.navigation.navigate('NewDeck')}
-        />
+
+        <View style={styles.btn}>
+          <Fab
+            style={{ backgroundColor: '#841584' }}
+            position="bottomRight"
+            onPress={() => this.props.navigation.navigate('NewDeck')}>
+            <Icon name="add" />
+          </Fab>
+        </View>
       </View>
     )
   }
@@ -48,11 +57,8 @@ class Dashboard extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
     paddingTop: 7,
+    flex: 1,
   },
   title: {
     fontSize: 16,
@@ -60,11 +66,13 @@ const styles = StyleSheet.create({
     textAlign: 'left',
   },
   decks: {
-    flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  btn: {
+    flex: 1,
   },
 });
 
